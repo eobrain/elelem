a3b4a4c68511292fc312ac49f6012b7c6485c601
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-04-11 05:34:09 +0000

    Allow it to use more tokens

--- llm.js
+++ llm.js
@@ -23,7 +23,7 @@ ${thread}
 
 @elelem:`,
         temperature: 0.9,
-        max_tokens: 150,
+        max_tokens: 500,
         top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.6,
