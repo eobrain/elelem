f3dfd678e3fb05707b29f891a4dc3a8a83635537
Eamonn O'Brien-Strain <eob@well.com>
2023-04-15 15:44:06 -0700

    Add comment to exported function

--- mastodon.js
+++ mastodon.js
@@ -1,84 +1,86 @@
-import { pp } from "passprint";
-import { compile } from "html-to-text";
+import { pp } from 'passprint'
+import { compile } from 'html-to-text'
 
-const accessToken = process.env.MASTODON_ACCESS_TOKEN;
-const baseUrl = process.env.MASTODON_BASE_URL;
+const accessToken = process.env.MASTODON_ACCESS_TOKEN
+const baseUrl = process.env.MASTODON_BASE_URL
 
 const headers = {
-  Authorization: `Bearer ${accessToken}`,
-};
+  Authorization: `Bearer ${accessToken}`
+}
 
 const notifications = async () => {
   const result = await (
     await fetch(pp(`${baseUrl}/api/v1/notifications/`), { headers })
-  ).json();
+  ).json()
   if (result.error) {
-    throw new Error(result.error);
+    throw new Error(result.error)
   }
-  return result;
-};
+  return result
+}
 
 /** Dismiss the notification so that it will not be returned in the next call to mentions() */
 export const dismissNotification = async (notificationId) => {
   await fetch(pp(`${baseUrl}/api/v1/notifications/${notificationId}/dismiss`), {
-    method: "POST",
-    headers,
-  });
-};
+    method: 'POST',
+    headers
+  })
+}
 
 const getToot = async (id) => {
   const result = await (
     await fetch(pp(`${baseUrl}/api/v1/statuses/${id}`), { headers })
-  ).json();
+  ).json()
   if (result.error) {
-    throw new Error(result.error);
+    throw new Error(result.error)
   }
   return {
     statusId: result.id,
     acct: result.account.acct,
     inReplyToId: result.in_reply_to_id,
-    text: convert(result.content),
-  };
-};
+    text: convert(result.content)
+  }
+}
 
 /** Post a response */
 export const toot = async (status, inReplyToId) => {
-  const body = new URLSearchParams();
-  body.append("status", status);
-  body.append("in_reply_to_id", inReplyToId);
-  pp(body);
+  const body = new URLSearchParams()
+  body.append('status', status)
+  body.append('in_reply_to_id', inReplyToId)
+  pp(body)
   await fetch(pp(`${baseUrl}/api/v1/statuses`), {
-    method: "POST",
+    method: 'POST',
     headers,
-    body,
-  });
-};
+    body
+  })
+}
 
+/** Return a string that concatetes the text for all the posts
+ * in the thread ending with the given post */
 export const assembleThread = async (post) => {
-  let thread = `@${post.acct}: ${post.text}`;
+  let thread = `@${post.acct}: ${post.text}`
   while (post.inReplyToId) {
-    const parentPost = await getToot(post.inReplyToId);
-    thread = `@${parentPost.acct}: ${parentPost.text}\n\n${thread}`;
-    post = parentPost;
+    const parentPost = await getToot(post.inReplyToId)
+    thread = `@${parentPost.acct}: ${parentPost.text}\n\n${thread}`
+    post = parentPost
   }
-  return thread;
-};
+  return thread
+}
 
-const INCLUDE_TYPES = ["mention"];
+const INCLUDE_TYPES = ['mention']
 
 const convert = compile({
   selectors: [
-    { selector: "a", options: { ignoreHref: true } },
-    { selector: "nav", format: "skip" },
-    { selector: "button", format: "skip" },
-    { selector: "form", format: "skip" },
-    { selector: "script", format: "skip" },
-    { selector: "style", format: "skip" },
-    { selector: "header", format: "skip" },
-    { selector: "footer", format: "skip" },
-    { selector: "template", format: "skip" },
-  ],
-});
+    { selector: 'a', options: { ignoreHref: true } },
+    { selector: 'nav', format: 'skip' },
+    { selector: 'button', format: 'skip' },
+    { selector: 'form', format: 'skip' },
+    { selector: 'script', format: 'skip' },
+    { selector: 'style', format: 'skip' },
+    { selector: 'header', format: 'skip' },
+    { selector: 'footer', format: 'skip' },
+    { selector: 'template', format: 'skip' }
+  ]
+})
 
 const filteredStatus = (status) =>
   !status.sensitive &&
@@ -87,29 +89,29 @@ const filteredStatus = (status) =>
     status.media_attachments.length === 0 ||
     status.media_attachments.every((m) => !!m.description)) &&
   !status.poll &&
-  (!status.card || status.card.type === "link");
+  (!status.card || status.card.type === 'link')
 
 const statusText = async (status) => {
-  let result = convert(status.content);
+  let result = convert(status.content)
   if (status.media_attachments) {
     for (const media of status.media_attachments) {
       if (media.description) {
-        result += `\n${media.description}`;
+        result += `\n${media.description}`
       }
     }
   }
   if (status.card) {
     if (status.card.title) {
-      result += `\n${status.card.title}`;
+      result += `\n${status.card.title}`
     }
     if (status.card.description) {
-      result += `\n${status.card.description}`;
+      result += `\n${status.card.description}`
     }
-    const linkContent = convert(await (await fetch(status.card.url)).text());
-    result += `\n${linkContent}`;
+    const linkContent = convert(await (await fetch(status.card.url)).text())
+    result += `\n${linkContent}`
   }
-  return result;
-};
+  return result
+}
 
 /** Return an array of all mentions, where each mention is {notificationId, acct, text} */
 export const mentions = async () =>
@@ -128,6 +130,6 @@ export const mentions = async () =>
         statusId: n.status.id,
         acct: n.status.account.acct,
         inReplyToId: n.status.in_reply_to_id,
-        text: await statusText(n.status),
+        text: await statusText(n.status)
       }))
-  );
+  )
