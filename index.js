7d6a65de4bfdefdf7f95813df048b1414ca87f63
Eamonn O'Brien-Strain <eob@well.com>
2023-04-16 10:43:24 -0700

    Be fair across users, so one user does not dominate respones.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import {
 } from './mastodon.js'
 import { pp } from 'passprint'
 
+const randomAlement = (xs) => xs[Math.floor(Math.random() * xs.length)]
+
 async function main () {
   const posts = pp(await mentions())
 
@@ -15,8 +17,21 @@ async function main () {
     return
   }
 
-  // Randomly select a post to reply to
-  const post = posts[Math.floor(Math.random() * posts.length)]
+  // Group posts by acct
+  const postsPerAcct = {}
+  for (const post of posts) {
+    if (post.acct in Object.keys(postsPerAcct)) {
+      postsPerAcct[post.acct].push(post)
+    } else {
+      postsPerAcct[post.acct] = [post]
+    }
+  }
+
+  // Randomly select a user to reply to
+  const acct = randomAlement(Object.keys(postsPerAcct))
+
+  // Randomly select a post from that user
+  const post = randomAlement(postsPerAcct[acct])
 
   const thread = await assembleThread(post)
 
@@ -27,8 +42,8 @@ async function main () {
     return
   }
 
-  console.log(`toot(${response}), ${post.statusId}, ${post.acct}`)
-  await toot(response, post.statusId, post.acct)
+  console.log(`toot(${response}), ${post.statusId}`)
+  await toot(response, post.statusId)
 
   await dismissNotification(pp(post.notificationId))
 }
